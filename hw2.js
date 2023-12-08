29095d9d0c9c1772b9ed4ee8d315c3ea1bd9d564
DimaGorenko <86059562+DimaGorenko@users.noreply.github.com>
2023-12-08 14:19:26 +0200

    added hw2.js

---
+++ hw2.js
@@ -0,0 +1,51 @@
+// String
+
+const myName = "Dmytro";
+
+console.log("My name is " + myName);
+
+// Number
+
+const myAge = 23;
+
+console.log(`I am ${myAge} years old`);
+
+// Boolean
+
+const myInfoTrue = true;
+
+console.log(`Am I ${myAge} years old? ${myInfoTrue}`);
+
+// undefined
+
+const userName = undefined;
+
+console.log(`Name of user: ooops, it's ${userName}`);
+
+// null
+
+const userAge = null;
+
+console.log(`Age of user: ooops, it's ${userAge}`);
+
+// object
+
+const mySummaryInfo = {
+  name: myName,
+  age: myAge,
+  isItTrue: myInfoTrue,
+};
+
+console.log(mySummaryInfo);
+
+// BigInt
+
+const bigInt = 2932832n;
+
+console.log(bigInt);
+
+// symbol
+
+const symbol = Symbol();
+
+console.log(symbol);
