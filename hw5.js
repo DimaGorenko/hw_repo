8a7b5830d32d7facf93403be172abde68e86e23c
DimaGorenko <86059562+DimaGorenko@users.noreply.github.com>
2023-12-19 15:38:57 +0200

    added hw5.js

---
+++ hw5.js
@@ -0,0 +1,56 @@
+// створити 3 обʼєкти через {}
+
+const dmytro = { sex: "male" };
+const anastasiia = { key2: "female" };
+const kostyantyn = { key3: "male" };
+
+// створити 3 обʼєкти через Object()
+
+const serhii = new Object({ sex: "male" });
+const nataliia = new Object({ sex: "female" });
+const yevhenii = new Object({ sex: "male" });
+
+// створити обʼєкт на базі протиту іншого обʼєку (Object.create(інший_оʼєкт), або через __prototype__),
+// створити одному в кожному форматі
+
+const myObject = { age: 24 };
+const notMyObject = Object.create(myObject);
+
+const myNewObject = {
+  age: 25,
+};
+
+const notMyNewObject = {};
+notMyNewObject.__proto__ = myNewObject;
+
+// створити обʼєкт engineer, від нього унаслідувати QA_engineer, вивести деталі по всім полям (engineer, QA_engineer)
+
+const engineer = {
+  name: "Dmytro",
+  age: 25,
+};
+
+const QA_engineer = Object.create(engineer);
+
+console.log(engineer);
+console.log(QA_engineer);
+
+// унаслідувати ваш існуючий engineer від person. вивести значення всіх 3х обʼєктів
+
+const person = {
+  name: "Anastasiia",
+};
+
+Object.setPrototypeOf(engineer, person);
+
+console.log(engineer);
+console.log(QA_engineer);
+console.log(person);
+
+// Рішення задачки з leetcode
+
+const merge = function (nums1, m, nums2) {
+  nums1.splice(m);
+  nums1.push(...nums2);
+  nums1.sort((a, b) => a - b);
+};
